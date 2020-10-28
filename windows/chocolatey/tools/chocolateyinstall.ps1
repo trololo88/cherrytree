62e45edc643a7fdcb2eddb2fb24b9b4ea9a9fb6b
John Smith <ao123123@ya.ru>
2020-10-28 22:17:24 +0300

    version up

--- windows/chocolatey/tools/chocolateyinstall.ps1
+++ windows/chocolatey/tools/chocolateyinstall.ps1
@@ -1,13 +1,13 @@
 ﻿$packageName= 'cherrytree'
 $toolsDir   = "$(Split-Path -Parent $MyInvocation.MyCommand.Definition)"
-$url        = 'https://www.giuspen.com/software/cherrytree_0.99.16.0_setup.exe'
+$url        = 'https://www.giuspen.com/software/cherrytree_0.99.17.0_setup.exe'
 
 $packageArgs = @{
   packageName   = $packageName
   url           = $url
   silentArgs    = "/s /S /q /Q /quiet /silent /SILENT /VERYSILENT"
   softwareName  = 'Cherrytree*'
-  checksum      = '7dec833759ddb582d992884f2c66b4fce58353c1ca527571d5ed36b16bed61d4'
+  checksum      = 'e72d1b6d45aca84c3c8f5bd77a346ac4ae54adf3ada31c3e0b0364f201a2580a'
   checksumType  = 'sha256'
 }
 
